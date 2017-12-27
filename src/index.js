d88f7cc25fa5f5106ed9a1c2d617ae7cff907b79
tonychangcheng <cheng.chang2@pactera.com>
2017-12-27 15:46:34 +1100

    moving up

--- src/index.js
+++ src/index.js
@@ -7,58 +7,60 @@ function Cell(props) {
     return React.createElement('div', {className: 'map-cell'}, props.value);
 }
 
-class GameMap extends React.Component {
-    render() {
-        return React.createElement(
-            'div', {className: 'game-map'},
-            React.createElement(
-                'div', {className: 'map-row'},
-                React.createElement(Cell, {value: 'A'}),
-                React.createElement(Cell),
-                React.createElement(Cell),
-                React.createElement(Cell)
-            ),
-            React.createElement(
-                'div', {className: 'map-row'},
-                React.createElement(Cell),
-                React.createElement(Cell),
-                React.createElement(Cell),
-                React.createElement(Cell)
-            ),
-            React.createElement(
-                'div', {className: 'map-row'},
-                React.createElement(Cell),
-                React.createElement(Cell),
-                React.createElement(Cell),
-                React.createElement(Cell)
-            ),
-            React.createElement(
-                'div', {className: 'map-row'},
-                React.createElement(Cell),
-                React.createElement(Cell),
-                React.createElement(Cell),
-                React.createElement(Cell)
-            )
-        );
+function GameMap(props) {
+    let rows = [];
+    for (let y = 0; y < props.value.mapSize.y; y++) {
+        let cells = [];
+        for (let x = 0; x < props.value.mapSize.x; x++) {
+            let text = '';
+            if (x === props.value.robotPosition.x && y === props.value.robotPosition.y) {
+                text = props.value.robotPosition.direction;
+            }
+
+            cells.push(React.createElement(Cell, {value: text, key: x}));
+        }
+        rows.push(React.createElement('div', {className: 'map-row', key: y}, cells));
     }
+    return React.createElement('div', {className: 'game-map'}, rows);
 }
 
-class GameControl extends React.Component {
-    render() {
-        return React.createElement(
-            'div', {className: 'game-control'},
-            React.createElement('button', {id: 'command-up'}, '^')
-        )
-    }
+function GameControl(props) {
+    return React.createElement(
+        'div', {className: 'game-control'},
+        React.createElement('button', {onClick: props.onCommandUp}, '^')
+    )
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mapSize: {x: 4, y: 4},
+            robotPosition: {x: 0, y: 3, direction: 'A'}
+        }
+    }
+    availablePosition(position, mapSize) {
+        if (position.x >=0 && position.x < mapSize.x && position.y >=0 && position.y < mapSize.y) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    onCommandUp = () => {
+        let targetPosition = {...this.state.robotPosition, ...{y: this.state.robotPosition.y - 1}};
+        if (this.availablePosition(targetPosition, this.state.mapSize)) {
+            this.setState({...this.state, ...{robotPosition: targetPosition}});
+        } else {
+            alert('Could not move.')
+        }
+    }
     render() {
         return React.createElement(
             'div',
             {className: 'game'},
-            React.createElement(GameMap),
-            React.createElement(GameControl)
+            React.createElement(GameMap, {value: this.state}),
+            React.createElement(GameControl, {onCommandUp: this.onCommandUp}
+            )
         );
     }
 }
